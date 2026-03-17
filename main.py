0d1cf03750b6409b58c0aa01b8c4d18c864235a6
Chinara Guliyeva <ichinara@gmail.com>
2026-03-18 00:37:39 +0400

    edited req, added first api

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class AIRequest(BaseModel):
+    prompt: str
+
+@app.get("/")
+def read_root():
+    return {"status": "ok", "message": "Backend is running"}
+
+# @app.post("/ask")
+# async def ask_ai(request: AIRequest):
+#     user_text = request.prompt
+#     ai_response = f"
+
+#     return {"response": ai_response}
