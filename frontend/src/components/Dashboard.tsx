78bead64ffbf47d5865cafb5f76dfe5636a455dd
Chinara Guliyeva <ichinara@gmail.com>
2026-03-19 22:07:29 +0400

    Added template components

---
+++ frontend/src/components/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface DashboardProps {
+
+}
+
+export const Dashboard = ({ }: DashboardProps) => {
+    return (
+        <div>
+            Hello ai-stylist
+        </div>
+    );
+};
---
+++ frontend/src/components/Profile.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface ProfileProps {
+
+}
+
+export const Profile = ({ }: ProfileProps) => {
+    return (
+        <div>
+            Hello profile
+        </div>
+    );
+};
