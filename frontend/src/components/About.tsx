2851338b69b18a43db6fbb912366180f3c05bba9
Chinara Guliyeva <ichinara@gmail.com>
2026-03-24 16:33:43 +0400

    Added photo

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -1,10 +1,11 @@
 import { aboutHeading, aboutText } from "../textConstants";
+import photo from "../assets/photo.jpg"
 
 export const About = () => {
     return (
         <div className="d-flex">
             <div className="col-md-6 ms-5">
-                {/* <img className="dashboard-img" src="#" /> */}
+                <img className="about-img" src={photo} />
             </div>
             <div className="col-md-6 description">
                 <h1 className="mb-3">{aboutHeading}</h1>
